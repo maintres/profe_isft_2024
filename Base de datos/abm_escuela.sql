b691960869583eb620c5926a3c316bc592c9a324
Demian <demianagustinds3@gmail.com>
2024-07-30 14:50:51 -0300

    BD actualizada y modulo Dicta actualizado con filtro de busqueda para saber si esta inactivo o activo , como asi tambien para  el modulo materia.

--- Base de datos/abm_escuela.sql
+++ Base de datos/abm_escuela.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 18-07-2024 a las 19:38:12
+-- Tiempo de generación: 30-07-2024 a las 19:48:26
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -30,17 +30,9 @@ SET time_zone = "+00:00";
 CREATE TABLE `asignaturas` (
   `id` int(11) NOT NULL,
   `nombre` varchar(255) NOT NULL,
-  `cantidaddehoras` int(11) NOT NULL
+  `cantidaddehoras` int(11) NOT NULL,
+  `etapa` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Volcado de datos para la tabla `asignaturas`
---
-
-INSERT INTO `asignaturas` (`id`, `nombre`, `cantidaddehoras`) VALUES
-(3, 'Matemáticas', 50),
-(4, 'Lengua Española', 40),
-(5, 'Ciencias Naturales', 45);
 
 -- --------------------------------------------------------
 
@@ -52,34 +44,9 @@ CREATE TABLE `asistencias` (
   `id` int(11) NOT NULL,
   `profesor_id` int(11) NOT NULL,
   `fecha` date NOT NULL,
-  `estado` varchar(25) NOT NULL
+  `estado` varchar(25) NOT NULL,
+  `etapa` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Volcado de datos para la tabla `asistencias`
---
-
-INSERT INTO `asistencias` (`id`, `profesor_id`, `fecha`, `estado`) VALUES
-(1, 33, '2023-11-16', 'presente'),
-(2, 40, '2023-11-16', 'presente'),
-(3, 47, '2023-11-24', 'justificado'),
-(4, 40, '2023-11-24', 'ausente'),
-(5, 47, '2023-11-25', 'ausente'),
-(6, 61, '2023-11-16', 'justificado'),
-(7, 40, '2023-11-16', 'presente'),
-(8, 58, '2023-11-16', 'presente'),
-(9, 51, '2023-12-10', 'presente'),
-(10, 33, '2023-12-10', 'justificado'),
-(11, 33, '2023-12-10', 'justificado'),
-(12, 33, '2023-12-29', 'justificado'),
-(13, 59, '2023-11-16', 'justificado'),
-(14, 58, '2023-12-01', 'presente'),
-(15, 46, '2023-11-16', 'ausente'),
-(16, 40, '2023-11-16', 'presente'),
-(17, 51, '2023-11-17', 'presente'),
-(18, 50, '2023-11-18', 'presente'),
-(19, 93, '2023-11-24', 'presente'),
-(20, 94, '2023-11-24', 'justificado');
 
 -- --------------------------------------------------------
 
@@ -90,17 +57,9 @@ INSERT INTO `asistencias` (`id`, `profesor_id`, `fecha`, `estado`) VALUES
 CREATE TABLE `carreras` (
   `id` int(11) NOT NULL,
   `nombre` varchar(255) NOT NULL,
-  `descripcion` varchar(255) NOT NULL
+  `descripcion` varchar(255) NOT NULL,
+  `etapa` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Volcado de datos para la tabla `carreras`
---
-
-INSERT INTO `carreras` (`id`, `nombre`, `descripcion`) VALUES
-(1, 'Carrera 1', 'Descripción de Carrera 1'),
-(2, 'Carrera 2', 'Descripción de Carrera 2'),
-(3, 'Carrera 3', 'Descripción de Carrera 3');
 
 -- --------------------------------------------------------
 
@@ -115,7 +74,8 @@ CREATE TABLE `dicta` (
   `tipo` enum('titular','interino','suplente') DEFAULT NULL,
   `Baja` enum('SI','NO') DEFAULT NULL,
   `Fecha_baja` date DEFAULT NULL,
-  `motivo_baja` varchar(255) DEFAULT NULL
+  `motivo_baja` varchar(255) DEFAULT NULL,
+  `etapa` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -130,7 +90,8 @@ CREATE TABLE `licencias` (
   `fechadeinicio` date NOT NULL,
   `fechadefin` date NOT NULL,
   `idprofesor` int(11) NOT NULL,
-  `idtipos_licencias` int(11) NOT NULL
+  `idtipos_licencias` int(11) NOT NULL,
+  `etapa` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -146,15 +107,6 @@ CREATE TABLE `permisos` (
   `fecha_creacion` datetime DEFAULT NULL,
   `fecha_modificacion` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `permisos`
---
-
-INSERT INTO `permisos` (`id`, `nombre`, `descripcion`, `fecha_creacion`, `fecha_modificacion`) VALUES
-(1, 'admin', 'Descripción del Permiso1', '2023-11-21 09:02:46', '2023-11-21 09:02:46'),
-(2, 'docente', 'Descripción del Permiso2', '2023-11-21 09:02:46', '2023-11-21 09:02:46'),
-(3, 'preceptor', 'Descripción del Permiso3', '2023-11-21 09:02:46', '2023-11-21 09:02:46');
 
 -- --------------------------------------------------------
 
@@ -172,58 +124,9 @@ CREATE TABLE `profesores` (
   `foto` varchar(255) NOT NULL,
   `cv` varchar(255) NOT NULL,
   `fechadeingreso` date NOT NULL,
-  `fechadebaja` date DEFAULT NULL
+  `fechadebaja` date DEFAULT NULL,
+  `etapa` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Volcado de datos para la tabla `profesores`
---
-
-INSERT INTO `profesores` (`id`, `nombreyapellido`, `dni`, `domicilio`, `telefono`, `email`, `foto`, `cv`, `fechadeingreso`, `fechadebaja`) VALUES
-(33, 'Kenny Pablo', '123123', '3333', '123123', 'admin@gmail.comASas', 'undefined', 'Captura de pantalla 2023-10-30 205651.png', '2023-12-02', '2023-11-12'),
-(40, 'Pablo Esteban', '123123', 'santa', '123123', 'admin@gmail.comASas', 'Captura de pantalla 2023-10-29 135658.png', 'Captura de pantalla 2023-10-30 205651.png', '2023-11-25', '2023-11-06'),
-(46, 'Juan Pérez', '12345678', 'Calle 123', '123456789', 'juanperez@email.com', 'foto.jpg', 'cv.pdf', '2023-11-12', NULL),
-(47, 'María González', '87654321', 'Calle 456', '987654321', 'mariagonzalez@email.com', 'foto2.jpg', 'cv2.pdf', '2023-11-11', NULL),
-(48, 'Pedro López', '34567890', 'Calle 789', '76543210', 'pedrolopez@email.com', 'foto3.jpg', 'cv3.pdf', '2023-11-10', NULL),
-(50, 'Roberto Martínez', '98765432', 'Calle 1213', '32109876', 'robertomarti@email.com', 'foto5.jpg', 'cv5.pdf', '2023-11-08', NULL),
-(51, 'Laura García', '76543210', 'Calle 1415', '10987654', 'lauragarcia@email.com', 'foto6.jpg', 'cv6.pdf', '2023-11-07', NULL),
-(52, 'David Fernández', '54321098', 'Calle 1617', '98765432', 'davidfernan@email.com', 'foto7.jpg', 'cv7.pdf', '2023-11-06', NULL),
-(53, 'Cristina Álvarez', '32109876', 'Calle 1819', '76543210', 'cristinaalv@email.com', 'foto8.jpg', 'cv8.pdf', '2023-11-05', NULL),
-(55, 'Isabel Romero', '98765432', 'Calle 2223', '32109876', 'isabelromero@email.com', 'foto10.jpg', 'cv10.pdf', '2023-11-03', NULL),
-(56, 'Luis Moreno', '76543210', 'Calle 2425', '10987654', 'luismoreno@email.com', 'foto11.jpg', 'cv11.pdf', '2023-11-02', NULL),
-(57, 'Anabel Ruiz', '54321098', 'Calle 2627', '98765432', 'anabelruiz@email.com', 'foto12.jpg', 'cv12.pdf', '2023-11-01', NULL),
-(58, 'Javier López', '32109876', 'Calle 2829', '76543210', 'javierlopez@email.com', 'foto13.jpg', 'cv13.pdf', '2023-10-31', NULL),
-(59, 'Marta Gómez', '10987654', 'Calle 3031', '54321098', 'martagomez@email.com', 'foto14.jpg', 'cv14.pdf', '2023-10-30', NULL),
-(60, 'Ruben Acosta', '123123', 'santa', '123123', 'admin@gmail.comASas', 'Captura de pantalla 2023-10-29 145952.png', 'Captura de pantalla 2023-11-01 102306.png', '2023-11-12', '2023-12-10'),
-(61, 'fabian Acosta', '123123', 'santa', '123123', 'admin@gmail.comASas', 'Captura de pantalla 2023-10-29 135658.png', 'Captura de pantalla 2023-10-29 134813.png', '2023-11-12', '2023-11-12'),
-(62, 'Pablo ramirez', '123123', 'asd', '123123', 'admin@gmail.comASas', 'Captura de pantalla 2023-10-29 134813.png', 'Captura de pantalla 2023-10-29 145952.png', '2023-11-12', '2023-11-26'),
-(63, 'Pablo ramirez', '123123', 'asd', '123123', 'admin@gmail.comASas', 'Captura de pantalla 2023-10-29 134813.png', 'Captura de pantalla 2023-10-29 145952.png', '2023-11-12', '2023-11-26'),
-(64, 'Pablo ramirezs', '123123', 'asd', '123123', 'admin@gmail.comASas', '[object Object]', '[object Object]', '2023-11-12', '2023-12-09'),
-(65, 'Fernando Jimenez', '31355346', 'Barrio prueba', '123123', 'admin@gmail.comASas', 'Captura de pantalla 2023-10-29 145952.png', 'Captura de pantalla 2023-10-29 145952.png', '2023-11-16', '2023-12-10'),
-(66, 'asddddasd', '31355346', 'Barrio prueba', '123123', 'admin@gmail.comASas', 'Captura de pantalla 2023-10-29 134813.png', 'Captura de pantalla 2023-10-29 135658.png', '2023-11-16', '2023-11-16'),
-(71, 'Pablo fabian', '31355346', 'santa', '123123', 'admin@gmail.comASas', '[object Object]', '', '2023-11-18', '2023-11-05'),
-(72, 'Pablo fabian', '31355346', 'santa', '123123', 'admin@gmail.comASas', '[object Object]', '', '2023-11-18', '2023-11-05'),
-(73, 'Pablo fabian', '31355346', 'santa', '123123', 'admin@gmail.comASas', '[object Object]', '', '2023-11-18', '2023-11-05'),
-(74, 'Pablo fabian', '31355346', 'santa', '123123', 'admin@gmail.comASas', '[object Object]', '', '2023-11-18', '2023-11-05'),
-(75, 'Pablo fabian', '31355346', 'santa', '123123', 'admin@gmail.comASas', '[object Object]', '', '2023-11-18', '2023-11-05'),
-(76, 'Pablo fabian', '31355346', 'santa', '123123', 'admin@gmail.comASas', '[object Object]', '', '2023-11-18', '2023-11-18'),
-(77, 'Pablo fabian', '31355346', 'santa', '123123', 'admin@gmail.comASas', '[object Object]', '', '2023-11-18', '2023-11-18'),
-(78, 'Pablo fabian', '31355346', 'santa', '123123', 'admin@gmail.comASas', '[object Object]', '', '2023-11-18', '2023-11-18'),
-(79, 'Pablo fabian', '31355346', 'santa', '123123', 'admin@gmail.comASas', '[object Object]', '', '2023-11-18', '2023-11-18'),
-(80, 'Pablo fabian', '31355346', 'santa', '123123', 'admin@gmail.comASas', '[object Object]', '', '2023-11-18', '2023-11-18'),
-(81, 'Pablo fabian', '31355346', 'santa', '123123', 'admin@gmail.comASas', '[object Object]', '', '2023-11-18', '2023-11-18'),
-(83, 'fjimenes', '31355346', 'Barrio prueba', '123123', 'admin@gmail.comASas', '[object Object]', '[object Object]', '2023-11-18', '2023-11-18'),
-(84, 'fjimenes', '31355346', 'Barrio prueba', '123123', 'admin@gmail.comASas', '[object Object]', '[object Object]', '2023-11-18', '2023-11-18'),
-(85, 'edaurdo', '123123', 'Barrio prueba', '123123', 'admin@gmail.comASas', 'Captura de pantalla 2023-10-29 134813.png', '[object Object]', '2023-11-19', '2023-11-19'),
-(86, 'edaurdoaaa', '123123', 'Barrio prueba', '123123', 'admin@gmail.comASas', '[object Object]', '[object Object]', '2023-11-07', '2023-11-21'),
-(88, 'edaurdoaaa', '123123', 'Barrio prueba', '123123', 'admin@gmail.comASas', 'Captura de pantalla 2023-11-01 094544.png', '[object Object]', '2023-11-07', '2023-11-21'),
-(90, 'Fernando Jimenezyyyyyyyyyyyyyyy', '123123', 'Barrio prueba', '123123', 'admin@gmail.comASas', 'undefined', '[object Object]', '2023-11-22', '2023-11-21'),
-(93, 'Pablo Esteban', '', '', '', '', 'Captura de pantalla 2023-11-01 094544.png', '[object Object]', '2023-11-21', '2023-11-21'),
-(94, 'Pablo Esteban acosta', '', 'Barrio prueba', '123123', 'admin@gmail.comASas', 'Captura de pantalla 2023-11-01 094544.png', '[object Object]', '2023-11-21', '2023-11-21'),
-(95, 'prueba', '31355346', '3333', '123123', 'admin@gmail.comASas', 'Captura de pantalla 2023-10-29 134813.png', '[object Object]', '2023-11-21', '2023-11-21'),
-(96, 'prueba5', '31355346', '3333', '123123', 'admin@gmail.comASas', 'Captura de pantalla 2023-10-29 134813.png', '[object Object]', '2023-11-21', '2023-11-21'),
-(97, 'prueba5', '31355346', '3333', '123123', 'admin@gmail.comASas', 'Captura de pantalla 2023-10-29 134813.png', '[object Object]', '2023-11-21', '2023-11-21'),
-(98, 'prueba5', '31355346', '3333', '123123', 'admin@gmail.comASas', 'Captura de pantalla 2023-10-29 134813.png', '[object Object]', '2023-11-21', '2023-11-21');
 
 -- --------------------------------------------------------
 
@@ -237,13 +140,6 @@ CREATE TABLE `profesores_asignaturas` (
   `cantidad_horas` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `profesores_asignaturas`
---
-
-INSERT INTO `profesores_asignaturas` (`id_profesor`, `id_asignatura`, `cantidad_horas`) VALUES
-(33, 3, 50);
-
 -- --------------------------------------------------------
 
 --
@@ -254,13 +150,6 @@ CREATE TABLE `profesores_carreras` (
   `id_profesor` int(11) NOT NULL,
   `id_carrera` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `profesores_carreras`
---
-
-INSERT INTO `profesores_carreras` (`id_profesor`, `id_carrera`) VALUES
-(33, 2);
 
 -- --------------------------------------------------------
 
@@ -382,25 +271,25 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `asignaturas`
 --
 ALTER TABLE `asignaturas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `asistencias`
 --
 ALTER TABLE `asistencias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `carreras`
 --
 ALTER TABLE `carreras`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `dicta`
 --
 ALTER TABLE `dicta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `licencias`
@@ -412,13 +301,13 @@ ALTER TABLE `licencias`
 -- AUTO_INCREMENT de la tabla `permisos`
 --
 ALTER TABLE `permisos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `profesores`
 --
 ALTER TABLE `profesores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `tipos_licencias`
@@ -469,12 +358,6 @@ ALTER TABLE `profesores_asignaturas`
 ALTER TABLE `profesores_carreras`
   ADD CONSTRAINT `profesores_carreras_FK_1` FOREIGN KEY (`id_profesor`) REFERENCES `profesores` (`id`),
   ADD CONSTRAINT `profesores_carreras_FK_2` FOREIGN KEY (`id_carrera`) REFERENCES `carreras` (`id`);
-
---
--- Filtros para la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD CONSTRAINT `usuarios_FK` FOREIGN KEY (`id_permisos`) REFERENCES `permisos` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- Views/admin/materia_crea.php
+++ Views/admin/materia_crea.php
@@ -1,22 +1,26 @@
 <?php
 include '../../conn/connection.php';
+
 // Verifica si se envió una solicitud POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Recolecta datos del formulario
     $nombre = trim($_POST["nombre"]);
     $cantidaddehoras = trim($_POST["cantidaddehoras"]);
     $error = "";
+
     try {
-        // Verificar si el correo electrónico ya existe
+        // Verificar si el nombre de la materia ya existe
         $sql_check_nombre = "SELECT COUNT(*) FROM asignaturas WHERE nombre = :nombre";
         $stmt_check_nombre = $db->prepare($sql_check_nombre);
         $stmt_check_nombre->bindParam(':nombre', $nombre);
         $stmt_check_nombre->execute();
         $count_nombre = $stmt_check_nombre->fetchColumn();
-        // Verificar si el DNI ya existe        
+
+        // Verificar si el nombre ya existe        
         if ($count_nombre > 0) {
             $error = "Materia ya registrada. Por favor, ingrese una diferente.";
         }
+
         if ($error) {
             $redirect_url = "materia_crea.php?error=" . urlencode($error)
                 . "&nombre=" . urlencode($nombre)
@@ -25,11 +29,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             exit();
         } else {
             // Inserta datos en la base de datos
-            $sql = "INSERT INTO asignaturas (nombre,cantidaddehoras) 
-                    VALUES (:nombre, :cantidaddehoras)";
+            $sql = "INSERT INTO asignaturas (nombre, cantidaddehoras, etapa) 
+                    VALUES (:nombre, :cantidaddehoras, 'Activo')";
             $stmt = $db->prepare($sql);
             $stmt->bindParam(':nombre', $nombre);
-            $stmt->bindParam(':cantidaddehoras', $cantidaddehoras);     
+            $stmt->bindParam(':cantidaddehoras', $cantidaddehoras);  
+            
             if ($stmt->execute()) {
                 header("Location: materia_index.php?mensaje=" . urlencode("Materia ingresada con éxito."));
                 exit();
@@ -47,7 +52,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
-<!-- ---------------------------------------------------- -->
+
 <?php require 'navbar.php'; ?>
 <div class="container mt-3">
     <div class="card rounded-2 border-0">
@@ -60,21 +65,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $cantidaddehoras = isset($_GET["cantidaddehoras"]) ? $_GET["cantidaddehoras"] : "";            
             ?>
             <form id="formulario" method="post" action="">
-                <!-- --------------------------------- -->
-                    <div class="col">
-                        <div class="form-group">
-                            <label for="nombre">Nombre:</label>
-                            <input type="text" class="form-control" name="nombre" id="nombre" value="<?php echo htmlspecialchars($nombre); ?>" autocomplete="off" placeholder="Ingrese Nombre(s)" required>
-                        </div>
-                        <!-- ---------------------------- -->
-                        <div class="form-group">
-                            <label for="cantidaddehoras">Horas de cursada:</label>
-                            <input type="text" class="form-control" name="cantidaddehoras" id="cantidaddehoras" value="<?php echo htmlspecialchars($cantidaddehoras); ?>" autocomplete="off" placeholder="Ingrese cantidaddehoras(s)" required>
-                        </div>          
+                <div class="col">
+                    <div class="form-group">
+                        <label for="nombre">Nombre:</label>
+                        <input type="text" class="form-control" name="nombre" id="nombre" value="<?php echo htmlspecialchars($nombre); ?>" autocomplete="off" placeholder="Ingrese Nombre(s)" required>
                     </div>
-                <!-- --------------------------------- -->
+                    <div class="form-group">
+                        <label for="cantidaddehoras">Horas de cursada:</label>
+                        <input type="text" class="form-control" name="cantidaddehoras" id="cantidaddehoras" value="<?php echo htmlspecialchars($cantidaddehoras); ?>" autocomplete="off" placeholder="Ingrese cantidad de horas" required>
+                    </div>          
+                </div>
                 <button type="button" class="btn btn-primary float-right" id="guardarBtn" onclick="validarFormulario()">Guardar</button>
-                <!-- Agregamos un div para mostrar un mensaje de confirmación -->
                 <div id="confirmacion" style="display: none;">
                     <p>¿Estás seguro de que deseas guardar los datos?</p>
                     <button type="button" class="btn btn-success" id="confirmarBtn">Sí</button>
