f02f5ef1df6a5efbd09295fc395f5f9f270a7a69
agustin <agustingarcia370@gmail.com>
2024-08-05 19:58:40 -0300

    descargar asistencia pdf I/L

--- conn/connection.php
+++ conn/connection.php
@@ -1,17 +1,23 @@
-<!-- base datos cami -->
 <?php
 // Variables de conexión a la base de datos
 $db_host = 'localhost'; // Host de la base de datos
 $db_name = 'abm_escuela'; // Nombre de la base de datos
 $db_user = 'root'; // Usuario de la base de datos
-$db_password = ''; // Contraseña de la base de datos
+$db_password = '44061051'; // Contraseña de la base de datos
 
 try {
     $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo "Error de conexión: " . $e->getMessage();
+    error_log("Error de conexión: " . $e->getMessage()); // Registro del error
+    exit(); // Finaliza el script sin generar salida
 }
+
 $conexion = new mysqli($db_host, $db_user, $db_password, $db_name);
+if ($conexion->connect_error) {
+    error_log("Error de conexión: " . $conexion->connect_error); // Registro del error
+    exit(); // Finaliza el script sin generar salida
+}
 ?>
+
 
